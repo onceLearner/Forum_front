a4b0bd78942cf658633131b5f0e324c671514114
OnceLearner <redaamir972@gmail.com>
2021-01-04 10:56:45 +0100

    new cmmit from local 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import { Router } from "@reach/router"
 import SimplePeer from "simple-peer";
-import Home from "./components/Home";
-import Peerjs from "./components/Peerjs";
-import SocketConn from "./components/SocketConn"
-import WaitHere from "./components/WaitHere"
+import Home from "./views/Home";
+
 
 
 function App() {
   return (
     <Router>
       <Home path="/" />
-      <SimplePeer path="/simple" />
-      <Peerjs path="/peerjs" />
-      <SocketConn path="/socket" />
-      <WaitHere path="/socke" />
     </Router>
 
   );
---
+++ src/views/Home.js
@@ -0,0 +1,14 @@
+import { Link } from '@reach/router'
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <h1 className="text-5xl text-center  p-32">
+                Welcome we are building .....
+            </h1>
+        </div>
+    )
+}
+
+export default Home
